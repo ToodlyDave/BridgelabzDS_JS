68f1e3eb6527d3d56506b2cf290a057263c347b6
ToodlyDave <alapatdavid@gmail.com>
2021-12-29 13:10:33 +0530

    UPDATE program to sort array first

--- Array Practice Problems/ThreeDigit.js
+++ Array Practice Problems/ThreeDigit.js
@@ -5,23 +5,5 @@ third = Math.floor(Math.random() * 1000);
 array = [first, second, third];
 console.log(array)
 
-if (first > second && first > third) {
-    if (second < third)
-        console.log(third);
-    else
-        console.log(second);
-}
-
-else if (second > first && second > third) {
-    if (first < third)
-        console.log(third);
-    else
-        console.log(first);
-}
-
-else {
-    if (first < second)
-        console.log(second);
-    else
-        console.log(first);
-}
+array.sort();
+console.log(array[1]);
